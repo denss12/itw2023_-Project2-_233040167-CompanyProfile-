825710d9bf4905afb50812957ca93823845a0f4f
denss12 <145624652+denss12@users.noreply.github.com>
2024-01-07 00:50:23 +0700

    Add files via upload

---
+++ js/cod.js
@@ -0,0 +1,8 @@
+function showCOD() {
+    document.getElementById("codColumn").style.display = "flex";
+}
+
+// Function to hide COD Column
+function hideCOD() {
+    document.getElementById("codColumn").style.display = "none";
+}
